b0673a8473ca18ac2a76235931f072a29c53a5bb
Dominika Gnich <d.gnich@wp.pl>
2017-06-10 09:38:40 +0200

    init

---
+++ js/zmienne.js
@@ -0,0 +1,16 @@
+'use strict';
+'use script'
+
+var imie;
+var imieDamskie = "Kaja";
+var wiek = 37;
+
+var kolor = "czerwony";
+
+imie = "Sergio";
+
+console.log(imie);
+console.log(wiek);
+console.log(kolor);
+
+
